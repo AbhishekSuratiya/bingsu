0308667fa3f4b701dfb3ecf40ab114c77e75c9fd
AbhishekSuratiya <abhishek.suratiya@makeen.io>
2022-10-06 09:21:55 +0530

    Added loading indicator in learnmore

--- src/components/screens/LearnMoreScreen/LearnMoreScreen.js
+++ src/components/screens/LearnMoreScreen/LearnMoreScreen.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import WebView from 'react-native-webview';
 import { LEARN_MORE_LINK } from '../../../utils/contants';
+import { ActivityIndicator } from 'react-native';
+import Colors from '../../../theme/Colors';
 
-const LearnMoreScreen = () => <WebView source={{ uri: LEARN_MORE_LINK }} />;
+const LearnMoreScreen = () => (
+  <WebView
+    source={{ uri: LEARN_MORE_LINK }}
+    renderLoading={() => {
+      return <ActivityIndicator color={Colors.blue} size="large" />;
+    }}
+  />
+);
 
 export default LearnMoreScreen;
