9d852f804f1415823db61c88d5644c06e7ca0cd0
AbhishekSuratiya <abhishek.suratiya@makeen.io>
2022-10-24 10:00:03 +0530

    Sending wifi status to sitewise

--- src/components/organisms/WifiStatus/WifiStatus.js
+++ src/components/organisms/WifiStatus/WifiStatus.js
@@ -1,21 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Switch, Text, View } from 'react-native';
 import styles from '../WifiStatus/WifiStatusStyles';
 import Collapsible from 'react-native-collapsible';
 import { SENSOR_CARD_HEADER } from '../../../utils/contants';
 import Colors from '../../../theme/Colors';
 import { useNetInfo } from '@react-native-community/netinfo';
+import { useSelector } from 'react-redux';
+import { BatchPutAssetPropertyValueCommand } from '@aws-sdk/client-iotsitewise';
+import { AwsContext } from '../../../containers/InitialiseAws';
 
-const WifiStatus = props => {
+const WifiStatus = () => {
   const [isSensorListening, setIsSensorListening] = useState(false);
   const [isConnected, setIsConnected] = useState(false);
   const [strength, setStrength] = useState(false);
   const netInfo = useNetInfo();
+  const {
+    isAwsConnected,
+    qrData: { ASSET_MODEL_MEASUREMENTS_PREFIX },
+  } = useSelector(state => state.awsStore);
+  const client = useContext(AwsContext);
 
   useEffect(() => {
     setIsConnected(netInfo?.type === 'wifi' ? 'Yes' : 'No');
     setStrength(netInfo?.details?.strength || 0);
-  }, [netInfo]);
+    if (isAwsConnected && isSensorListening) {
+      const command = new BatchPutAssetPropertyValueCommand({
+        entries: [
+          {
+            entryId: 'WifiSignalsMeasurement',
+            propertyAlias:
+              ASSET_MODEL_MEASUREMENTS_PREFIX + 'WifiSignalsMeasurement',
+            propertyValues: [
+              {
+                value: { doubleValue: netInfo?.details?.strength },
+                timestamp: { timeInSeconds: Date.now() / 1000 },
+              },
+            ],
+          },
+          {
+            entryId: 'WifiConnectMeasurement',
+            propertyAlias:
+              ASSET_MODEL_MEASUREMENTS_PREFIX + 'WifiConnectMeasurement',
+            propertyValues: [
+              {
+                value: { stringValue: netInfo?.type === 'wifi' ? 'Yes' : 'No' },
+                timestamp: { timeInSeconds: Date.now() / 1000 },
+              },
+            ],
+          },
+        ],
+      });
+      client?.send(command);
+    }
+  }, [netInfo, isSensorListening, isAwsConnected]);
 
   return (
     <View style={styles.root}>
