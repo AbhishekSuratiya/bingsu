2e8e29a6a8f6d04b0dc0a2aa40cffd6d49786d1f
AbhishekSuratiya <abhishek.suratiya@makeen.io>
2022-10-24 12:09:00 +0530

    Sending battery status to sitewise

--- src/components/organisms/BatteryLevel/BatteryLevel.js
+++ src/components/organisms/BatteryLevel/BatteryLevel.js
@@ -1,10 +1,13 @@
-import React, { useMemo, useState } from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 import { Switch, Text, View } from 'react-native';
 import styles from '../BatteryLevel/BatteryLevelStyles';
 import Collapsible from 'react-native-collapsible';
 import { SENSOR_CARD_HEADER } from '../../../utils/contants';
 import Colors from '../../../theme/Colors';
 import { usePowerState } from 'react-native-device-info';
+import { BatchPutAssetPropertyValueCommand } from '@aws-sdk/client-iotsitewise';
+import { useSelector } from 'react-redux';
+import { AwsContext } from '../../../containers/InitialiseAws';
 
 const BatteryLevel = props => {
   const [isSensorListening, setIsSensorListening] = useState(false);
@@ -13,6 +16,51 @@ const BatteryLevel = props => {
     () => (powerState?.batteryLevel * 100).toFixed(2),
     [powerState],
   );
+  const {
+    isAwsConnected,
+    qrData: { ASSET_MODEL_MEASUREMENTS_PREFIX },
+  } = useSelector(state => state.awsStore);
+  const client = useContext(AwsContext);
+
+  useEffect(() => {
+    if (isAwsConnected && isSensorListening) {
+      const command = new BatchPutAssetPropertyValueCommand({
+        entries: [
+          {
+            entryId: 'BatteryLevelMeasurement',
+            propertyAlias:
+              ASSET_MODEL_MEASUREMENTS_PREFIX + 'BatteryLevelMeasurement',
+            propertyValues: [
+              {
+                value: {
+                  doubleValue: powerState?.batteryLevel * 100,
+                },
+                timestamp: { timeInSeconds: Date.now() / 1000 },
+              },
+            ],
+          },
+          {
+            entryId: 'ChargingStatusMeasurement',
+            propertyAlias:
+              ASSET_MODEL_MEASUREMENTS_PREFIX + 'ChargingStatusMeasurement',
+            propertyValues: [
+              {
+                value: {
+                  stringValue: ['charging', 'full'].includes(
+                    powerState?.batteryState,
+                  )
+                    ? 'Charging'
+                    : 'Not charging',
+                },
+                timestamp: { timeInSeconds: Date.now() / 1000 },
+              },
+            ],
+          },
+        ],
+      });
+      client?.send(command);
+    }
+  }, [powerState, isAwsConnected, isSensorListening]);
 
   return (
     <View style={styles.root}>
@@ -43,10 +91,11 @@ const BatteryLevel = props => {
                   styles.dataTextColored,
                 ]}>{`${batteryLevel}%`}</Text>
             </View>
-            <Text
-              style={
-                styles.dataText
-              }>{`Charging: ${powerState?.batteryState?.toUpperCase()}`}</Text>
+            <Text style={styles.dataText}>{`Charging: ${
+              ['charging', 'full'].includes(powerState?.batteryState)
+                ? 'Yes'
+                : 'No'
+            }`}</Text>
           </View>
 
           <View style={styles.batteryLevelWrapper}>
